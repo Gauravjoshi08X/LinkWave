894e7e0db82246d82f2401918359e7729fa5f8db
Gaurav Joshi <121346271+Gauravjoshi08X@users.noreply.github.com>
2026-05-15 21:16:04 +0545

    Update config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://YOUR_USERNAME.github.io',
+  site: 'https://gauravjoshi08x.github.io',
   base: '/LinkWave/',
   output: 'static'
 });
