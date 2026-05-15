1443c79e5a5dfd60b38597e3c2a3221f0ddf89e7
Gaurav Joshi <121346271+Gauravjoshi08X@users.noreply.github.com>
2026-05-15 20:50:34 +0545

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://gauravjoshi08X.github.io', // Replace with your GitHub username
-  base: '/', // Replace with your repository name
-  output: 'static',
-  build: {
-    assets: 'assets'
-  }
+  site: 'https://YOUR_USERNAME.github.io',
+  base: '/LinkWave/',
+  output: 'static'
 });
