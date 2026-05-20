3c11b5a242abcb8d8dce6191ba45e104faa5fb64
Gaurav Joshi <121346271+Gauravjoshi08X@users.noreply.github.com>
2026-05-21 01:55:18 +0545

    removed base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://linkwaveedu.com',
-  base: '/LinkWave/',
   output: 'static',
   integrations: [sitemap()]
 });
