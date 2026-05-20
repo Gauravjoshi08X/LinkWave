2c9e3290f1f8f7bf2a7e40f3c739d759b2ad3cb3
Gaurav Joshi <121346271+Gauravjoshi08X@users.noreply.github.com>
2026-05-20 19:43:12 +0545

    Hosting

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'linkwaveedu.com',
+  site: 'https://linkwaveedu.com',
   base: '/LinkWave/',
   output: 'static',
   integrations: [sitemap()]
