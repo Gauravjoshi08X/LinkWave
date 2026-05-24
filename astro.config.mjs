bc96721f362fd8fc6db76251801c59ccf484bd2a
Gaurav Joshi <121346271+Gauravjoshi08X@users.noreply.github.com>
2026-05-24 11:53:02 +0545

    SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,15 @@ export default defineConfig({
   site: 'https://linkwaveedu.com',
   base: '/',
   output: 'static',
-  integrations: [sitemap()]
+  integrations: [sitemap()],
+  build: {
+    inlineStylesheets: 'auto',
+    assets: 'assets'
+  },
+  vite: {
+    build: {
+      cssMinify: true,
+      assetsInlineLimit: 4096
+    }
+  }
 });
